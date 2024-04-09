f3db5c84020efaebb7abe5e74a97aac40c4ec3a5
Mussie Alemayehu <mussiealemayehu00@gmail.com>
2024-04-09 08:33:14 +0300

    finish the UI of the authentication screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -14,10 +14,80 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: const Column(
-        children: [
-          CustomAppBar(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const CustomAppBar(),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                children: [
+                  const SizedBox(height: 32),
+                  Text(
+                    '፬ ኪሎ ግቢ ጉባኤ',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  TextField(
+                    decoration: _decor(
+                      'መለያ ቁጥር',
+                      const Icon(Icons.person),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    decoration: _decor(
+                      'የይለፍ ቃል',
+                      const Icon(Icons.visibility_off),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: Theme.of(context).colorScheme.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Login'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _decor(String hint, Icon icon) {
+    return InputDecoration(
+      labelText: hint,
+      labelStyle: TextStyle(color: Theme.of(context).colorScheme.tertiary),
+      suffixIcon: icon,
+      suffixIconColor: Theme.of(context).colorScheme.tertiary,
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.tertiary,
+        ),
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Theme.of(context).primaryColor,
+          width: 3,
+        ),
       ),
     );
   }
